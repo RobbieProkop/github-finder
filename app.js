6cb1843f04e7c2ef8f6efeae4d325fa3792ca361
Robbie Prokop <robbieprokop@gmail.com>
2022-10-26 14:28:32 +0400

    beign appjs work

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // main app file
+
+// Search INput
+const searchUser = document.getElementById("searchUser");
+
+searchUser.addEventListener("keyup", (e) => {
+  // get input text
+  const userText = e.target.value;
+
+  if (!userText) {
+    return;
+  }
+  console.log("user text", userText);
+});
