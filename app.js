d26c892472dc40c1bac3e4cb50d5c01df3b3d257
Robbie Prokop <robbieprokop@gmail.com>
2022-10-26 20:37:38 +0400

    create and clear alert

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ searchUser.addEventListener("keyup", (e) => {
   github.getUser(userText).then((data) => {
     if (data.profile.message === "Not Found") {
       //Show Alert
-      return;
+      ui.showAlert("User not found", "alert alert-danger");
     }
     //Show Profile
     ui.showProfile(data.profile);
--- ui.js
+++ ui.js
@@ -15,10 +15,10 @@ class UI {
             </div>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
-            <span class="badge badge-primary">Following: ${user.following}</span>
+            <span class="badge rounded-pill bg-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge rounded-pill bg-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge rounded-pill bg-success">Followers: ${user.followers}</span>
+            <span class="badge rounded-pill bg-primary">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
               <li class="list-group-item">Company: ${user.company}</li>
@@ -34,6 +34,40 @@ class UI {
     `;
   }
 
+  //SHow alert for when user is not found
+  showAlert(message, className) {
+    // clear old alerts
+    this.clearAlert();
+    //creat div
+    const div = document.createElement("div");
+    div.className = className;
+    console.log("div", div);
+
+    //add text
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector(".searchContainer");
+    // get searchbox
+    const search = document.querySelector(".search");
+    //insert alert
+    container.insertBefore(div, search);
+
+    //timeout after 3 seconds
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  //method to clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  // clear the proile when no text is present in searchbox
   clearProfile() {
     this.profile.innerHTML = "";
   }
